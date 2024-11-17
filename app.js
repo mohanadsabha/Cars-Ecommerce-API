6b22fb94808003d506ec4ec728e6f6d7d45f216c
Mohanad Abu Sabha <eng.mohanadsabha@gmail.com>
2024-11-17 21:23:11 +0300

    Added Security Middlewares

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sabitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalEerrorHandler = require('./controllers/errorController');
 const userRouter = require('./routers/userRoutes');
 const carRouter = require('./routers/carRouter');
 
 const app = express();
+const limiter = rateLimit({
+    max: 100, // 100 req from same ip
+    windowMs: 60 * 60 * 1000, // in one hour
+    message: 'Too many requests from this IP, please try again in an hour',
+});
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Middlewares
-app.use(express.json());
+// Security Middlewares
+app.use(helmet());
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
 // Routes
 app.use('/api/v1/tours', carRouter);
