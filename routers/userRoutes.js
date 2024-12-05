aaaf435e36960729095fdbd8d98f066565ddfcaf
Mohanad Abu Sabha <eng.mohanadsabha@gmail.com>
2024-12-05 13:17:09 +0300

    Reviews Model

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -6,17 +6,25 @@ const router = express.Router();
 
 router.post('/signup', authContoller.signup);
 router.post('/login', authContoller.login);
-
 router.post('/forgotPassword', authContoller.forgotPassword);
 router.patch('/resetPassword/:token', authContoller.resetPassword);
 
-router.patch(
-    '/updateMyPassword',
-    authContoller.protect,
-    authContoller.updatePassword
-);
+// The below routes will be protected
+router.use(authContoller.protect);
 
-router.patch('/updateMe', authContoller.protect, userContoller.updateMe);
-router.delete('/deleteMe', authContoller.protect, userContoller.deleteMe);
+router.get('/me', userContoller.getMe, userContoller.getUser);
+router.patch('/updateMyPassword', authContoller.updatePassword);
+router.patch('/updateMe', userContoller.updateMe);
+router.delete('/deleteMe', userContoller.deleteMe);
+
+// Admin Only
+router.use(authContoller.restrictTo('admin'));
+
+router.route('/').get(userContoller.getAllUsers).post(userContoller.createUser);
+router
+    .route('/:id')
+    .get(userContoller.getUser)
+    .patch(userContoller.updateUser)
+    .delete(userContoller.deleteUser);
 
 module.exports = router;
